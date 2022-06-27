82ffdffc2227fd1d6a8bcc40282522e96cab8c92
Tomer Harari <tharari21@gmail.com>
2022-06-27 18:00:52 -0400

    Added pokemon as grid

--- main.js
+++ main.js
@@ -3,6 +3,9 @@
 // command + option + i to open inspect in chrome
 const pokemon = ["charmander", "bulbasaur", "squirtle"];
 const pokemonIDs = ["001", "004", "007"];
+// Value will be null if there is no element with id container.
+const containerDiv = document.querySelector("#container");
+
 // Loop over every ID,
 // create HTML element
 // Set HTML element values
@@ -10,9 +13,12 @@ const pokemonIDs = ["001", "004", "007"];
 pokemonIDs.map((id) => {
   let imageUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`;
   console.log(imageUrl);
+  let div = document.createElement("div"); // Adding a div to house the img element
+  //   document.setAttribute("class", "pokemon-card");
+  div.className = "pokemon-card"; // Setting the class of the div
   let img = document.createElement("img");
   img.src = imageUrl;
-  img.setAttribute("height", "100px");
-  img.setAttribute("width", "100px");
-  document.body.append(img);
+  div.append(img);
+  containerDiv.append(div);
+  //   document.querySelector(".pokemon-list").append(img);
 });
