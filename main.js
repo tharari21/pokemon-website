dede2fff5337a0cd5d042d797d38db17954d81b1
Tomer Harari <tharari21@gmail.com>
2022-06-28 11:23:31 -0400

    Created pokemon object rather than arrays and added name to pokemon images

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 // control + ` to open command prompt
 // Command + \ to open and close the file toolbar on the left
 // command + option + i to open inspect in chrome
-const pokemon = ["charmander", "bulbasaur", "squirtle"];
-const pokemonIDs = ["001", "004", "007"];
+const pokemonArr = [
+  { id: "001", name: "Bulbasaur" },
+  { id: "004", name: "Charmander" },
+  { id: "007", name: "Squirtle" },
+];
+
 // Value will be null if there is no element with id container.
 const containerDiv = document.querySelector("#container");
 
@@ -10,15 +14,17 @@ const containerDiv = document.querySelector("#container");
 // create HTML element
 // Set HTML element values
 // append HTML to the dom
-pokemonIDs.map((id) => {
-  let imageUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`;
-  console.log(imageUrl);
+pokemonArr.map((pokemon, index) => {
+  let imageUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemon.id}.png`;
+  console.log(pokemon.name);
   let div = document.createElement("div"); // Adding a div to house the img element
+  let h3 = document.createElement("h3");
+  h3.innerText = pokemon.name;
   //   document.setAttribute("class", "pokemon-card");
   div.className = "pokemon-card"; // Setting the class of the div
   let img = document.createElement("img");
   img.src = imageUrl;
-  div.append(img);
+  div.append(img, h3);
   containerDiv.append(div);
   //   document.querySelector(".pokemon-list").append(img);
 });
