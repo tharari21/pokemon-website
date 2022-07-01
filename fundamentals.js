2237667ba95a95888d26f51b700436c0e6873ffe
Tomer Harari <tharari21@gmail.com>
2022-07-01 11:45:37 -0400

    fundamentals with michael complete

--- fundamentals.js
+++ fundamentals.js
@@ -1,3 +1,6 @@
+const container = document.querySelector("#container");
+const p = document.querySelector("#para");
+
 // Method 1
 let li = document.createElement("li");
 li.textContent = "Apple";
@@ -21,3 +24,31 @@ const createLi = (textContent) => {
 list1.append(createLi("Apple"));
 list1.append(createLi("Cucumber"));
 list1.append(createLi("Guitar"));
+
+// create clickable button that will change the text of the paragraph element with id of `para` to say `button was clicked`
+let btn = document.createElement("button");
+btn.textContent = "Click Me";
+btn.addEventListener("click", () => {
+  p.textContent = "Button was clicked!";
+});
+container.append(btn);
+// Create an input element that accepts textinput. Then also create a button that says "submit" The input element will have the id of textinput. The button will have the id of text-input-button.
+const input1 = document.createElement("input");
+input1.type = "text"; // Default so not necessary
+input1.id = "textinput";
+btn = document.createElement("input");
+btn.type = "submit";
+btn.textContent = "Submit";
+btn.id = "text-input-button";
+const input2 = document.createElement("input");
+input2.type = "text";
+
+container.append(input1, input2, btn);
+
+//When submit button is clicked, take the text in the inout field and creeate an HTML element that matched the kind of
+btn.addEventListener("click", () => {
+  let elementType = input1.value;
+  let newElement = document.createElement(elementType);
+  newElement.textContent = input2.value;
+  container.append(newElement);
+});
